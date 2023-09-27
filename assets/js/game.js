1057aea769c997348ef8ed00eac54c3b0810a6c4
TT84 <tthompson8416@outlook.com>
2023-09-27 17:02:03 -0400

    Initial conditions set up

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,13 +19,30 @@ var enemyAttack = 12;
     enemyHealth = enemyHealth - playerAttack;
 
     //Log a resulting message to the console so we know that it worked.
-    console.log (playerName + " attacked " + enemyName + " . " + " now has " + enemyHealth + " health remaining ")
+    console.log (playerName + " attacked " + enemyName + " . " + " now has " + enemyHealth + " health remaining ");
+
+    // check enemy's health 
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died! ");
+    }
+
+    else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }
 
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-    playerHealth = playerAttack - enemyAttack;
+    playerHealth = playerHealth - enemyAttack;
 
     // Log a resulting message to the console so we know that it worked. 
     console.log(enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining ");
+     // check player's health
+     if (playerHealth <= 0) {
+      window.alert(playerHealth + " has died! ");
+     }
+
+     else {
+      window.alert(playerName + " still has " + playerHealth + " health left. ");
+     }
 
  };
 
